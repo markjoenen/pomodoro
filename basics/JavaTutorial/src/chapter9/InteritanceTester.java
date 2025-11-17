398a1b9cc7ecc5c7486d547d224a1beb5bb95687
markjoenen <markjoenen09@gmail.com>
2025-11-17 22:36:02 +0800

    my eyes are very heavy

---
+++ basics/JavaTutorial/src/chapter9/InteritanceTester.java
@@ -0,0 +1,29 @@
+package chapter9;
+
+public class InteritanceTester {
+    public static void main(String[] args) {
+//        Person person = new Person();
+//        Employee employee = new Employee();
+//        Rectangle rectangle = new Rectangle();
+//        rectangle.print();
+//
+//        Square square = new Square();
+//        square.print("square");
+        Mother mom = new Mother();
+        mom.setName("Brenda");
+
+        System.out.println();
+    }
+
+    public static void testSquareOverride(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(4);
+        rectangle.setWidth(8);
+        System.out.println(rectangle.calculatePerimeter());
+
+        Square square = new Square();
+        square.setLength(4);
+        square.setWidth(8);
+        System.out.println(square.calculatePerimeter());
+    }
+}
